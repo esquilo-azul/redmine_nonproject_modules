573c5e57df30dbd5b0106695feca376d2a341010
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-07-12 13:31:46 -0300

    Plugin's description.

---
+++ init.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+require 'redmine'
+
+Redmine::Plugin.register :redmine_trf1_sjap do
+  name 'Redmine mon-project modules'
+  author 'Eduardo Henrique Bogoni'
+  description 'Customizações para a SJAP'
+  version '0.0.1'
+  url 'https://github.com/eduardobogoni/redmine_nonproject_modules'
+  author_url 'https://github.com/eduardobogoni'
+end
