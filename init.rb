424b2a496732e0c7461706ac7e5795e875391c90
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2017-05-30 14:23:11 -0300

    User.permission?. Refs #1074.

--- init.rb
+++ init.rb
@@ -26,4 +26,5 @@ end
 Rails.configuration.to_prepare do
   GroupPermission.add_permission('group_permissions')
   GroupPermission.add_permission('group_merge')
+  require_dependency 'redmine_nonproject_modules/patches/user_patch'
 end
---
+++ lib/redmine_nonproject_modules/patches/user_patch.rb
@@ -0,0 +1,19 @@
+module RedmineNonprojectModules
+  module Patches
+    module UserPatch
+      def self.included(base)
+        base.include(InstanceMethods)
+      end
+
+      module InstanceMethods
+        def permission?(permissions)
+          GroupPermission.permission?(permissions, self)
+        end
+      end
+    end
+  end
+end
+
+unless ::User.included_modules.include?(RedmineNonprojectModules::Patches::UserPatch)
+  ::User.send(:include, RedmineNonprojectModules::Patches::UserPatch)
+end
