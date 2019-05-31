aaa28c7023b45e22db5452d2c5021bbb37b086c0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-05-31 14:29:37 -0300

    Fix date/time translation on Active Scaffold's views.

--- init.rb
+++ init.rb
@@ -6,6 +6,7 @@ require 'redmine'
 
 require 'redmine_nonproject_modules/patches/controller_patch'
 require 'redmine_nonproject_modules/patches/group_patch'
+require 'redmine_nonproject_modules/patches/redmine/i18n_patch'
 require 'redmine_nonproject_modules/patches/redmine/menu_manager_patch'
 
 Redmine::Plugin.register :redmine_nonproject_modules do
