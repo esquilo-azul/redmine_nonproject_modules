75fddcf9a62ef2eb664f9772c1f477fb7c32ee7d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-05-25 22:24:18 -0300

    RedmineNonprojectModules::Patches::Redmine::I18nPatch: replace "alias_method_chain" with "prepend".

--- lib/redmine_nonproject_modules/patches/redmine/i18n_patch.rb
+++ lib/redmine_nonproject_modules/patches/redmine/i18n_patch.rb
@@ -4,18 +4,11 @@ module RedmineNonprojectModules
   module Patches
     module Redmine
       module I18nPatch
-        def self.included(base)
-          base.include(InstanceMethods)
-          base.alias_method_chain :l, :active_scaffold
-        end
-
-        module InstanceMethods
-          def l_with_active_scaffold(*args)
-            if (args.first.is_a?(Time) || args.first.is_a?(Date)) && args.last.is_a?(Hash)
-              ::I18n.l(args.first, args.last)
-            else
-              l_without_active_scaffold(*args)
-            end
+        def l(*args)
+          if (args.first.is_a?(Time) || args.first.is_a?(Date)) && args.last.is_a?(Hash)
+            ::I18n.l(args.first, args.last)
+          else
+            super(*args)
           end
         end
       end
@@ -26,4 +19,4 @@ end
 x = ::Redmine::I18n
 y = ::RedmineNonprojectModules::Patches::Redmine::I18nPatch
 
-x.send(:include, y) unless x.included_modules.include?(y)
+x.send(:prepend, y) unless x.included_modules.include?(y)
