40fe26f4f6773410c813bdd4ac4d4c7ddacb64ae
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-06-26 13:58:19 -0300

    All migrations: adapt to Rails 4 or 5.

--- db/migrate/20160712162812_create_group_permissions.rb
+++ db/migrate/20160712162812_create_group_permissions.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-class CreateGroupPermissions < ActiveRecord::Migration
+class CreateGroupPermissions < (
+    Rails.version < '5.2' ? ActiveRecord::Migration : ActiveRecord::Migration[4.2]
+  )
   def change
     create_table :group_permissions do |t|
       t.references :group
