cfdb8024ab6676398b6eb417a9f5a09b0c4358c4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-05-05 00:24:36 -0300

    config/initializers/assets.rb: format with RuboCop.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,4 @@
-Rails.application.config.assets.precompile += %w( active_scaffold.css )
-Rails.application.config.assets.precompile += %w( active_scaffold.js )
+# frozen_string_literal: true
+
+Rails.application.config.assets.precompile += %w[active_scaffold.css]
+Rails.application.config.assets.precompile += %w[active_scaffold.js]
