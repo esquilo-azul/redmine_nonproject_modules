94c0ecc376d0d8213de6c39c7db4c08f68a55726
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-06-21 20:22:57 -0300

    config/initializers/001_patches.rb.

---
+++ config/initializers/001_patches.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'redmine_nonproject_modules/patches/controller_patch'
+require 'redmine_nonproject_modules/patches/group_patch'
+require 'redmine_nonproject_modules/patches/redmine/i18n_patch'
+require 'redmine_nonproject_modules/patches/redmine/menu_manager_patch'
+require 'redmine_nonproject_modules/patches/user_patch'
