dbd4b1bd99f13c4531d51e35d72512f47b1c8858
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-06-22 00:12:31 -0300

    RedmineNonprojectModules::Patches::Redmine::I18nPatch: fix.

--- lib/redmine_nonproject_modules/patches/redmine/i18n_patch.rb
+++ lib/redmine_nonproject_modules/patches/redmine/i18n_patch.rb
@@ -1,14 +1,21 @@
 # frozen_string_literal: true
 
+require 'redmine/i18n'
+
 module RedmineNonprojectModules
   module Patches
     module Redmine
       module I18nPatch
-        def l(*args)
+        common_concern do
+          alias_method :original_l, :l
+          alias_method :l, :fixed_l
+        end
+
+        def fixed_l(*args)
           if (args.first.is_a?(Time) || args.first.is_a?(Date)) && args.last.is_a?(Hash)
             ::I18n.l(args.first, args.last)
           else
-            super(*args)
+            original_l(*args)
           end
         end
       end
@@ -19,4 +26,4 @@ end
 x = ::Redmine::I18n
 y = ::RedmineNonprojectModules::Patches::Redmine::I18nPatch
 
-x.send(:prepend, y) unless x.included_modules.include?(y)
+x.send(:include, y) unless x.included_modules.include?(y)
