7ecc8a91bc24273c54dab18031ff5c6f138c1c46
Eduardo Henrique Bogoni <eduardo.bogoni@trf1.jus.br>
2023-09-14 18:40:33 -0300

    Plugin "redmine_plugins_helper": update to '>= 0.13'.

--- config/initializers/000_dependencies.rb
+++ config/initializers/000_dependencies.rb
@@ -2,5 +2,5 @@
 
 Redmine::Plugin.post_register :redmine_nonproject_modules do
   # Source: https://github.com/esquilo-azul/redmine_plugins_helper
-  requires_redmine_plugin(:redmine_plugins_helper, version_or_higher: '0.12')
+  requires_redmine_plugin(:redmine_plugins_helper, version_or_higher: '0.13')
 end
