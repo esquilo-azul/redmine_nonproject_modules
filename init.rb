b780fe894247ffaa865dbe6f69989704d1bf8859
Eduardo Henrique Bogoni <eduardo.bogoni@trf1.jus.br>
2023-10-16 23:06:06 -0300

    init.rb: remove patches' requires.

--- init.rb
+++ init.rb
@@ -4,11 +4,6 @@ require 'active_scaffold'
 require 'jquery/rails'
 
 require 'redmine'
-
-require 'redmine_nonproject_modules/patches/controller_patch'
-require 'redmine_nonproject_modules/patches/group_patch'
-require 'redmine_nonproject_modules/patches/redmine/i18n_patch'
-require 'redmine_nonproject_modules/patches/redmine/menu_manager_patch'
 require 'redmine_nonproject_modules/version'
 
 Redmine::Plugin.register :redmine_nonproject_modules do
